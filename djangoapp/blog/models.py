72c9680a6ca80b3be53621160506cbcde6808aa7
Guilherme Alves <muzzigui@icloud.com>
2025-08-22 11:43:38 +0100

    update views/listviews

--- djangoapp/blog/models.py
+++ djangoapp/blog/models.py
@@ -83,6 +83,11 @@ class Page(models.Model):
                                        )
     content = models.TextField()
 
+    def get_absolute_url(self):
+        if not self.is_published:
+            return reverse("blog:index")
+        return reverse("blog:page", kwargs={"slug": self.slug,})
+
     def save(self, *args, **kwargs):
         if not self.slug:
             self.slug = slugify_new(self.title, 4)  # Replace with actual slug generation logic
